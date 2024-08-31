a818f1c7444aca4e576b1c58d8e1f33b18e1e7ba
yurixander <101931215+yurixander@users.noreply.github.com>
2024-08-31 09:57:44 -0400

    feat: Add 'Create deposit' dialog

--- src/containers/DepositButton.tsx
+++ src/containers/DepositButton.tsx
@@ -4,6 +4,14 @@ import { FC } from "react";
 import Button from "../components/Button";
 import { FiPlusCircle } from "react-icons/fi";
 import { useAccount } from "wagmi";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { DialogFooter, DialogHeader } from "../components/Dialog";
 
 const DepositButton: FC = () => {
   const { isConnected } = useAccount();
@@ -11,12 +19,31 @@ const DepositButton: FC = () => {
   // TODO: Implement deposit logic.
 
   return (
-    <>
-      <Button disabled={!isConnected}>
-        <FiPlusCircle />
-        <span>Create a new deposit</span>
-      </Button>
-    </>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button disabled={!isConnected}>
+          <FiPlusCircle />
+          <span>Create a new deposit</span>
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a new deposit</DialogTitle>
+
+          <DialogDescription>
+            You&apos;re about to create a new deposit. This will lock your funds
+            for a certain period of time. After this period, you must manually
+            withdraw your funds, as they will not be automatically returned to
+            your wallet.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
